eca7dea458c4252d614a7ae3e7017d5712637972
Sanjay S <143824748+SSanjay0614@users.noreply.github.com>
2026-04-02 22:22:20 +0530

    changes

--- Fall Detection Arduino Codes/fall-knn-model.ino
+++ Fall Detection Arduino Codes/fall-knn-model.ino
@@ -29,12 +29,12 @@ float angle_buffer[25];
 void score(double * input, double * output) {
     double var0[2];
 
-    if (input[1] <= 2.0) {
-        if (input[6] > 1.5) {
-            if (input[0] > 11.0) {
-                if (input[7] > 0.5) {
-                    if (input[4] > 60.0) {
-                        if (input[8] <= 15.0) {
+    if (input[1] <= 3.6754653) {
+        if (input[6] > 2.312645) {
+            if (input[0] > 11.492050) {
+                if (input[7] > 0.543256) {
+                    if (input[4] > 63.56845) {
+                        if (input[8] <= 17.153267) {
                             memcpy(var0, (double[]){0.0, 1.0}, 2 * sizeof(double));
                         } else {
                             memcpy(var0, (double[]){1.0, 0.0}, 2 * sizeof(double));
